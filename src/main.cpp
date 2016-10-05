2797c589385b669eb058cf501a4329619f00d06b
mkerin <m4tt.kerin@gmail.com>
2016-10-05 13:57:39 +0100

    Center & scale, and print max coeff

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,13 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <Eigen/Dense>
 #include <random>
 #include <vector>
 #include <string>
 #include <cstdio>
+#include <cstring>
 #include <cmath>
 
 // My functions
@@ -19,66 +21,140 @@
 // IMPROVEMENTS
 
 int main (int argc, char *argv[]) {
-  
-  
+  char env_filepath[40], trait_filepath[40], res_filepath[40];
   Eigen::MatrixXd X, Y, R, y_hat;
-  bool header = true;
-  //X = readMatrix("data/biobank_env.tab", X, header);
-  //Y = readMatrix("data/biobank_trait.tab", Y, header);
+  bool header_in_file = true;
   
-  X = readMatrix("data/for_matt_env.tab", X, header);
-  Y = readMatrix("data/for_matt_trait.tab", Y, header);
-  
-  std::cout << X << std::endl;
-  std::cout << Y << std::endl;
-  
-  R = X.transpose() * X;
-  y_hat = X.transpose() * Y;
-  
-  Eigen::MatrixXd beta_hat = R.ldlt().solve(y_hat);
-  std::cout << beta_hat << std::endl;
-  
-  /*
-  if(argc >= 1 && !strcmp(argv[1], "new")){
-    int n = atoi(argv[2]);
-    int g = atoi(argv[3]);
-    int t = atoi(argv[4]);
-    genData(n, g, t);
+  // command line arguments
+  if(argc > 1){
+    strcpy(env_filepath, argv[1]);
+    strcpy(trait_filepath, argv[2]);
+  }
+  else {
+    strcpy(env_filepath, "data/for_matt_env.tab");
+    strcpy(trait_filepath, "data/for_matt_trait.tab");
   }
   
-  // Reading in Matrix
-  Eigen::MatrixXi X;
-  Eigen::MatrixXd T, Y;
-  X = readMatrix("data/X_mat.txt", X);
-  Y = readMatrix("data/Y_mat.txt", Y);
-  T = readMatrix("data/T_mat.txt", T);
+  if(argc > 3) strcpy(res_filepath, argv[3]);
+  else strcpy(res_filepath, "data/mvlr_coefficients.txt");
+  
+  // reading data from file
+  X = readMatrix(env_filepath, X, header_in_file);
+  Y = readMatrix(trait_filepath, Y, header_in_file);
+  std::cout << "\nPrinting X" << std::endl;
+  std::cout << X << std::endl;
+  
+  // center and scale
+  
+  Eigen::RowVectorXd means = X.colwise().sum() / X.rows();
+  std::cout << "\nPrinting means" << std::endl;
+  std::cout << means << std::endl;
+  X.rowwise() -= means;
+  std::cout << "\nPrinting X" << std::endl;
+  std::cout << X << std::endl;
+  
+  Eigen::MatrixXd covs;
+  if(X.rows() > 1){
+    /*
+    std::cout << "\nPrinting sqs" << std::endl;
+    std::cout << (X.array() * X.array()) << std::endl;
+    
+    std::cout << "\nPrinting sum of sqs" << std::endl;
+    std::cout << (X.array() * X.array()).colwise().sum() << std::endl;
+    */
+    covs = (X.array() * X.array()).colwise().sum() / (X.rows() - 1);
+  } else {
+    covs = (X.array() * X.array()).colwise().sum() / (X.rows());
+  }
+  
+  //std::cout << "\nPrinting covs" << std::endl;
+  //std::cout << covs << std::endl;
+  
+  Eigen::MatrixXd sds = covs.array().sqrt();
+  //std::cout << "\nPrinting sds" << std::endl;
+  //std::cout << sds << std::endl;
+  
+  for(int i = 0; i < sds.size(); i++){
+    if(*(sds.data() + i) != 0) *(sds.data() + i) = 1 / *(sds.data() + i);
+  }
+  std::cout << "\nPrinting sds" << std::endl;
+  std::cout << sds << std::endl;
+  
+  X = (X * sds.asDiagonal()).eval();
+  std::cout << "\nPrinting X" << std::endl;
+  std::cout << X << std::endl;
   
   
-  // Quick look at data
-  std::cout << "Top left of X from file is: " << std::endl << X.topLeftCorner(5,5) << std::endl;
-  std::cout << "Top left of T from file is: " << std::endl << T.topLeftCorner(5,5) << std::endl;
-  
-  // MVLR of environmental covariates only
-  // b = (t(T)*T)^{-1}*t(T)*Y
-  Eigen::VectorXd beta, y_hat;
-  Eigen::MatrixXd R;
-  
+  // performing mvlr
+  printf("X: (%li, %li) \n", X.rows(), X.cols());
   R = X.transpose() * X;
   y_hat = X.transpose() * Y;
+  Eigen::MatrixXd beta_hat = R.ldlt().solve(y_hat);
+  //Eigen::MatrixXd beta_hat = Eigen::MatrixXd::Zero(X.rows(), X.cols());
   
-  MatrixXd beta_hat = R.ldlt().solve(y_hat);
   
+  // Output to console.
+  std::vector<std::string> covariates, traits;
+  std::string line, word;
+  std::stringstream lineStream;
   
-  beta = R.colPivHouseholderQr().solve(y_hat);
-  std::cout << "After using the colPivHouseholder, the first entries of beta are: " << std::endl << beta.topLeftCorner(5,1) << std::endl;  
+  std::ifstream infile(env_filepath);
+  if(infile.is_open()){
+    getline(infile, line);
+    lineStream.str(line);
+    puts(line.c_str());
+    while(lineStream >> word){
+      covariates.push_back(word);
+      puts(word.c_str());
+    }
+    lineStream.clear();
+    infile.close();
+  }
+  infile.open(trait_filepath);
+  if(infile.is_open()){
+    getline(infile, line);
+    lineStream.str(line);
+    puts(line.c_str());
+    while(lineStream >> word){
+      traits.push_back(word);
+      puts(word.c_str());
+    }
+    lineStream.clear();
+    infile.close();
+  }
   
-  beta = R.inverse() * y_hat;
-  std::cout << "After using brute force, the first entries of beta are:" << std::endl << beta.topLeftCorner(5,1) << std::endl;
+  std::cout << beta_hat << std::endl;
   
-  beta = R.llt().solve(y_hat);
-  std::cout << "After using llt method, the first entries of beta are: " << std::endl << beta.topLeftCorner(5,1) << std::endl;  
+  Eigen::MatrixXd::Index maxRow, maxCol;
+  //beta_hat.colwise().maxCoeff(&maxRow, &maxCol);
+  std::cout << "Max elements in row: " << beta_hat.colwise().maxCoeff() << std::endl;
+  
+  std::vector<Eigen::MatrixXd::Index> maxRows(beta_hat.cols());
+  std::vector<double> maxVals(beta_hat.cols());
+  for(int c = 0; c < beta_hat.cols(); c++){
+    maxVals[c] = beta_hat.col(c).maxCoeff(&(maxRows[c]));
+    printf("Max regression coefficient for trait %s is (%s, %f) \n",
+           traits[c].c_str(), covariates[maxRows[c]].c_str(), maxVals[c]);
+  }
+  
+  /*
+  std::vector<std::string>::iterator it;
+  for(it = traits.begin(); it != traits.end(); it++) std::cout << *it << " ";
+  std::cout << std::endl;
+  for(it = covariates.begin(); it != covariates.end(); it++) std::cout << *it << " ";
+  std::cout << std::endl;
   */
-   
+  // saving output to file.
+  infile.open(env_filepath);
+  std::ofstream outfile(res_filepath);
+  if(infile.is_open() && outfile.is_open()){
+    std::string line;
+    getline(infile, line);
+    outfile << line << std::endl;
+    beta_hat.transposeInPlace();
+    outfile << beta_hat << std::endl;
+    outfile.close();
+  }
   
   return 0;
 }
